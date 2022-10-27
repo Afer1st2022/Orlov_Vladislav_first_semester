ca88df5ac5082516fae86cd81d1d288e7d614eb2
Afer1st2022 <113258884+Afer1st2022@users.noreply.github.com>
2022-10-28 01:17:11 +0300

    Update 6 ПР.py

--- 6 ПР.py
+++ 6 ПР.py
@@ -1,10 +1,12 @@
-text = input('Введите текст: ')
-text1 = ''
-a = len(text) // 2
-for i in range(a):
-    if text[i] == 'п' or text[i] == 'П':
-        text1 += '*'
-    else: text1 += text[i]
-b = text[a:]
-text1 += b
-print(text1)
+def F():
+    text = input('Введите текст: ')
+    text1 = ''
+    a = len(text) // 2
+    for i in range(a):
+        if text[i] == 'п' or text[i] == 'П':
+            text1 += '*'
+        else: text1 += text[i]
+    b = text[a:]
+    text1 += b
+    print(text1)
+F()
